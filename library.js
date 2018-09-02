3ec87dff24b7072d8701295cd06b9832a9045489
Uthaeus <romanlavery@gmail.com>
2018-09-02 00:24:16 -0600

    Form functional and rendering mapped items

--- library.js
+++ library.js
@@ -1,4 +1,4 @@
-let title, author;
+
 
 const Library = {
   'The Stand': 'Stephen King',
@@ -7,18 +7,28 @@ const Library = {
   'A Tale of Two Cities': 'Charles Dickens'
 };
 
-titleUpdate = (event) => {
-  let title = event.target.value;
-}
-
-authorUpdate = (event) => {
-  author = event.target.value;
+libraryDisplay = () => {
+  return Object.keys(Library).map((key) => {
+    let node = document.createElement('div');
+    node.className = "libraryItem";
+    let head = document.createElement('h1');
+    let t = document.createTextNode(key);
+    head.appendChild(t);
+    let body = document.createElement('h4');
+    let a = document.createTextNode(Library[key]);
+    body.appendChild(a);
+    node.appendChild(head);
+    node.appendChild(body);
+    return document.getElementById('shelf').appendChild(node);
+  });
 }
 
 addToLibrary = () => {
-  let temp = new Book(title, author);
-  console.log(temp.info);
+  let title = document.getElementById('title').value;
+  let author = document.getElementById('author').value;
+  // let temp = new Book(title, author);
   Library[title] = author;
+  libraryDisplay();
 }
 
 class Book {
