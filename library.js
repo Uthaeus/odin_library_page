146de57a7e5e1880c6313bd7c59e72db7697e8a7
Uthaeus <romanlavery@gmail.com>
2018-09-13 19:11:08 -0600

    Functional page

--- library.js
+++ library.js
@@ -1,6 +1,6 @@
 
 
-const Library = [
+let Library = [
   {'Title': 'The Stand',
   'Author': 'Stephen King',
   'Pages': 1219},
@@ -16,6 +16,8 @@ const Library = [
 ];
 
 libraryDisplay = () => {
+  document.getElementById('shelf').innerHTML = '';
+
   return Library.map((book) => {
     let node = document.createElement('div');
     node.className = "libraryItem";
@@ -24,10 +26,29 @@ libraryDisplay = () => {
     head.appendChild(t);
     let body = document.createElement('h4');
     let a = document.createTextNode(book['Author']);
-    let p = document.createTextNode(book['Pages']);
-    body.appendChild(a, p);
+    let para = document.createElement('p');
+    let p = document.createTextNode(`${book['Pages']} pages`);
+    let checkPara = document.createElement('p');
+
+    let checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.name = 'read';
+    checkbox.value = 'read';
+    checkbox.id = 'readCheck';
+    let label = document.createElement('label');
+    label.htmlFor = 'readCheck';
+    label.appendChild(document.createTextNode('Book Read'));
+
+    checkPara.appendChild(label);
+    checkPara.appendChild(checkbox);
+
+    para.appendChild(p);
+    body.appendChild(a);
+    body.appendChild(para);
+    body.appendChild(checkPara);
     node.appendChild(head);
     node.appendChild(body);
+
     return document.getElementById('shelf').appendChild(node);
   });
 }
@@ -36,29 +57,33 @@ addToLibrary = () => {
   let title = document.getElementById('title').value;
   let author = document.getElementById('author').value;
   let pages = document.getElementById('pages').value;
-  // let temp = new Book(title, author);
-  let newAddition = [{'Title': title, 'Author': author, 'Pages': pages}];
-  Library = Library.concat(newAddition);
+  let newAddition = {'Title': title, 'Author': author, 'Pages': pages};
+  Library.push(newAddition);
+  clearForm();
   libraryDisplay();
 }
 
-class Book {
-  constructor(title, author) {
-    this.title = title;
-    this.author = author;
-    this.info = `Title: ${this.title}\nAuthor: ${this.author}`;
-  }
-
-  info() {
-    return this.info;
-  }
-
-  title() {
-    return this.title;
-  }
-  author() {
-    return this.author;
-  }
+clearForm = () => {
+  document.getElementById('form').reset();
 }
+
+// class Book {
+//   constructor(title, author) {
+//     this.title = title;
+//     this.author = author;
+//     this.info = `Title: ${this.title}\nAuthor: ${this.author}`;
+//   }
+
+//   info() {
+//     return this.info;
+//   }
+
+//   title() {
+//     return this.title;
+//   }
+//   author() {
+//     return this.author;
+//   }
+// }
 
 
